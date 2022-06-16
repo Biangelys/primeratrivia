3d3ef209e84f0fb754def60018d54c4848bcbb72
biantrix <103851152+Biangelys@users.noreply.github.com>
2022-06-15 22:54:44 -0400

    Add files via upload

---
+++ triviota.js
@@ -0,0 +1,67 @@
+//array con las respuestas correctas
+let correctas = [3, 2, 2, 2];
+
+//array donde vamos a guardar las respuesta del usuario
+let opcion_elegida = [];
+
+//función para tomar el numero de la pregunta y el input elegido de esa pregunta
+function respuesta(num_pregunta, seleccionada) {
+  //guardamos la respuesta elegida
+  opcion_elegida[num_pregunta] = seleccionada.value;
+
+  //el siguiente código es para poner en color blanco
+  //el fondo de los inputs para cuando elige otra opción
+  //armo el id para seleccionar el section correspondiente
+  id = "p" + num_pregunta;
+
+  labels = document.getElementById(id).childNodes;
+  labels[3].style.backgroundColor = "#FFFFFF80";
+  labels[5].style.backgroundColor = "#FFFFFF80";
+  labels[7].style.backgroundColor = "#FFFFFF80";
+
+  //doy el color a label seleccionado
+  seleccionada.parentNode.style.backgroundColor = "#FFFFFF80";
+}
+
+//funcion para comparar los arrays y saber cuantas estuvieron correctas
+
+function Resultados(){ 
+for(let i = 0; i < correctas.length; i++) {
+  const x = correctas[i];
+
+  console.log(correctas[i])
+}
+ }
+
+let resultado = document.getElementById("resultado");
+let b = document.getElementById("resultados");
+
+
+function corregir(){
+ var primera = document.getElementById("pc1");
+  primera.style.backgroundColor = "green";
+
+  var segunda = document.getElementById("pc2");
+  segunda.style.backgroundColor = "green";
+
+  var tercera = document.getElementById("pc3");
+  tercera.style.backgroundColor = "green";
+
+  var cuarta = document.getElementById("pc4");
+  cuarta.style.backgroundColor = "green";
+
+ }
+function resultados(){
+  var cinco = document.getElementById("rc1");
+  cinco.style.backgroundColor = "green";
+
+  var seis = document.getElementById("rc2");
+  seis.style.backgroundColor = "green";
+
+  var siete = document.getElementById("rc3");
+  siete.style.backgroundColor = "green";
+
+  var ocho = document.getElementById("rc4");
+  ocho.style.backgroundColor = "green";
+
+ }
